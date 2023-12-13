a7491e5852b36c1292eb44a1bb4e6e56681c0f50
SimonLof <simon.lofgren@yh.nackademin.se>
2023-12-13 16:50:20 +0100

    bas-lista för test

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const questionText = document.querySelector(".question"); // Frågans text
 const timer = document.querySelector(".time"); // Timern
 const answerBox = document.querySelector(".answers"); // Lådan som håller svarsknapparna
 const answerButtons = document.querySelectorAll(".alt"); // alla svarsknappar
+let lives = 3;
+
+const question1 = {
+  question: "Hur många är vi?",
+  answers: ["1", "2", "3", "4"],
+  correctAnswer: "4",
+};
+
+const question2 = {
+  question: "Sköldpaddor?",
+  answers: ["1", "2", "3", "4"],
+  correctAnswer: "4",
+};
+
+const questionArray = [];
+questionArray.push(question1);
+questionArray.push(question2);
+
+console.log(questionArray[0].answers);
