c43211e87b799c24f5c520bd1a05f48a79cf1780
SimonLof <lofgren.simon@gmail.com>
2023-12-13 19:14:37 +0100

    json-importen funkar. Och jag lade till en liten modul.

---
+++ modules/questionModule.mjs
@@ -0,0 +1,13 @@
+import allQuestionsOrdered from "/questions/qa.json" assert { type: "json" };
+let randomQuestionOrder = [];
+
+export function GetRandomQuestions(params) {
+  for (let i = 0; i < allQuestionsOrdered.length; i++) {
+    let randIndex = Math.floor(Math.random() * allQuestionsOrdered.length);
+    while (randomQuestionOrder.includes(allQuestionsOrdered[randIndex])) {
+      randIndex = Math.floor(Math.random() * allQuestionsOrdered.length);
+    }
+    randomQuestionOrder.push(allQuestionsOrdered[randIndex]);
+  }
+  return randomQuestionOrder;
+}
--- script.js
+++ script.js
@@ -1,24 +1,20 @@
+import { GetRandomQuestions } from "/modules/questionModule.mjs";
 const game = document.querySelector(".game"); // Hela gamerutan
 const questionText = document.querySelector(".question"); // Frågans text
 const timer = document.querySelector(".time"); // Timern
 const answerBox = document.querySelector(".answers"); // Lådan som håller svarsknapparna
 const answerButtons = document.querySelectorAll(".alt"); // alla svarsknappar
+
 let lives = 3;
 
-const question1 = {
-  question: "Hur många är vi?",
-  answers: ["1", "2", "3", "4"],
-  correctAnswer: "4",
-};
+// randomizedQuestions är den randomizerade ordningen på alla frågor.
+let randomizedQuestions = GetRandomQuestions();
+// currentQuestion är frågan man är på.
+let currentQuestion = randomizedQuestions[randomizedQuestions.length - 1];
 
-const question2 = {
-  question: "Sköldpaddor?",
-  answers: ["1", "2", "3", "4"],
-  correctAnswer: "4",
-};
-
-const questionArray = [];
-questionArray.push(question1);
-questionArray.push(question2);
-
-console.log(questionArray[0].answers);
+function AnswerQuestion() {
+  // Gör just nu bara att man tar bort sista frågan i arrayen, och sätter currentQuestion till nästa fråga.
+  // Här borde det kollas att det är rätt svar, m.m.
+  randomizedQuestions.pop();
+  currentQuestion = randomizedQuestions[randomizedQuestions.length - 1];
+}
