3051f0b34c1a3b9160e55d1d2289c293aaad430a
SimonLof <simon.lofgren@yh.nackademin.se>
2023-12-13 14:33:32 +0100

    create

---
+++ script.js
@@ -0,0 +1,5 @@
+const game = document.querySelector(".game");
+const questionText = document.querySelector(".question");
+const timer = document.querySelector(".time");
+const answerBox = document.querySelector(".answers");
+const answerButtons = document.querySelectorAll(".alt");
