179232c255755bf8f4d0b2d16cb19dc7d3e88a7a
SimonLof <simon.lofgren@yh.nackademin.se>
2023-12-15 09:51:27 +0100

    Progress bar

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const questionText = document.querySelector(".question"); // Frågans text
 const timer = document.querySelector(".time"); // Timern
 const answerBox = document.querySelector(".answers"); // Lådan som håller svarsknapparna
 const answerButtons = document.querySelectorAll(".alt"); // alla svarsknappar
-
+const timeBar = document.querySelector(".time-bar");
 let lives = 3;
 
+let timeToAnswer = 20;
 // randomizedQuestions är den randomizerade ordningen på alla frågor.
 let randomizedQuestions = GetRandomQuestions();
 // currentQuestion är frågan man är på.
@@ -25,3 +26,23 @@ console.log(
   currentQuestion.answers,
   currentQuestion.correctAnswer
 );
+console.log(timeBar);
+function StartTimer() {
+  let currentTime = timeToAnswer;
+  // Stoppa timern när man svarat på frågan.
+  const myTimer = setInterval(() => {
+    const newValue = `${(currentTime / timeToAnswer) * 100}`; // Få procent istället för att räkna på sekunder.
+    console.log(newValue);
+    timeBar.value = newValue;
+    currentTime -= 1 / 60;
+    if (currentTime <= 0) {
+      clearInterval(myTimer);
+      PlayerTimeOut();
+      alert(":(((((((((((");
+    }
+  }, 1000 / 60);
+}
+StartTimer();
+function PlayerTimeOut() {
+  // det som händer när man inte svarar i tid.
+}
